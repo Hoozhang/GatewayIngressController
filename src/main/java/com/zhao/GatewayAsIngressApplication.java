2068d50e8030392dd44922331756f23a49a9eda7
t-zhao <t-zhao@microsoft.com>
2020-09-09 17:17:01 +0800

    config SSL into code dynamically

--- src/main/java/com/zhao/GatewayAsIngressApplication.java
+++ src/main/java/com/zhao/GatewayAsIngressApplication.java
@@ -1,13 +1,28 @@
 package com.zhao;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class GatewayAsIngressApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayAsIngressApplication.class, args);
+
+		/*
+		// set server.ssl properties program
+		Properties properties = new Properties();
+		properties.put("server.ssl.key-alias", "1");
+		properties.put("server.ssl.key-password", "changeit");
+		properties.put("server.ssl.key-store", "classpath:self-signed-cert.p12");
+		properties.put("server.ssl.key-store-password", "changeit");
+		properties.put("server.ssl.key-store-type", "PKCS12");
+		new SpringApplicationBuilder(GatewayAsIngressApplication.class).properties(properties).run(args);
+		*/
 	}
 
 }
