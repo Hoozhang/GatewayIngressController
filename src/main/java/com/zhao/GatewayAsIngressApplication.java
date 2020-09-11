64096362ca66f2283c5212438a1ed452c926f685
t-zhao <t-zhao@microsoft.com>
2020-09-11 17:44:05 +0800

    run .pfx, update k8s/ directory

--- src/main/java/com/zhao/GatewayAsIngressApplication.java
+++ src/main/java/com/zhao/GatewayAsIngressApplication.java
@@ -1,7 +1,5 @@
 package com.zhao;
 
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -11,18 +9,24 @@ import java.util.Properties;
 public class GatewayAsIngressApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayAsIngressApplication.class, args);
+		//SpringApplication.run(GatewayAsIngressApplication.class, args);
 
-		/*
 		// set server.ssl properties program
 		Properties properties = new Properties();
-		properties.put("server.ssl.key-alias", "1");
-		properties.put("server.ssl.key-password", "changeit");
-		properties.put("server.ssl.key-store", "classpath:self-signed-cert.p12");
+
+		/*
+		 * for self-signed-cert.jks, key-store-password: "changeit"
+		 * for self-signed-cert.p12, key-store-password: "changeit"
+		 * for self-signed-cert.pfx, key-store-password: "changeit"
+		 * for ascdevkvshareda-customer-https-dev-20200911.pfx, key-store-password: ""
+		 * refer form https://docs.microsoft.com/en-us/azure/key-vault/certificates/how-to-export-certificate?tabs=azure-cli
+		 */
+		properties.put("server.ssl.key-store", "classpath:self-signed-cert.pfx");
 		properties.put("server.ssl.key-store-password", "changeit");
 		properties.put("server.ssl.key-store-type", "PKCS12");
+
 		new SpringApplicationBuilder(GatewayAsIngressApplication.class).properties(properties).run(args);
-		*/
+
 	}
 
 }
